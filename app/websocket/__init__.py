c56cd358f5c314b4042b9e91eb99760cd684a89c
Jakub Raj <kubaraj24@gmail.com>
2026-06-18 11:52:43 +0200

    Prepare release v1.0.0 fix

--- app/websocket/__init__.py
+++ app/websocket/__init__.py
@@ -1,3 +1,3 @@
 from .websockets import start_websockets, publish, publish_location, publish_packet
 
-__all__ = ["start_websockets", "publish", "publish_location", "publish_packet"]
+__all__ = ["start_websockets", "publish_location", "publish_packet"]
